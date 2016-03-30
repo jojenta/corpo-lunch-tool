9cb325b651193d724d2b273a4ad446bf4734abc1
gsaavedr <gino.saavedra@nokia.com>
2016-03-30 15:11:48 +0800

    remove comment

--- backend/Server.ts
+++ backend/Server.ts
@@ -31,19 +31,3 @@ export class Server {
     }
 }
 
-//var express = require('express');
-//var app = express();
-//var server = require('http').Server(app);
-//var io = require('socket.io')(server, {
-//    path: '/socket.io'
-//});
-//var socketServer = require('./backend/socketServer').connect(io);
-//var port = process.env.PORT || 8080;
-//
-//app.use(express.static('app'));
-//app.use(express.static('.'));
-//
-//server.listen(port, function(){
-//    console.log("Server started with port", port);
-//});
-
