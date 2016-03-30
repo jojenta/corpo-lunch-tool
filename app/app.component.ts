35d66a2c9fe4121e1cb004b78a5648d4aaf27751
Jobeth Jennifer Tampus <jobeth.tampus@nokia.com>
2016-03-30 16:43:35 +0800

    Added proposition form

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,15 @@
 import {Component, OnInit} from 'angular2/core';
+import {PropositionFormComponent} from './proposition-form.component'
 import {SocketHandlerService} from './socketHandler.service';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>',
+    template: '<h1>My First Angular 2 App</h1><proposition-form></proposition-form>',
+    directives: [PropositionFormComponent],
     providers: [SocketHandlerService]
 })
 export class AppComponent implements OnInit{
-    constructor(
-        private _socketHandlerService : SocketHandlerService ){}
+    constructor( private _socketHandlerService : SocketHandlerService ){}
     ngOnInit(){
         this._socketHandlerService.initialize();
     }
