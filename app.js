ea2e30903211784a6013d74639bd909d9886df9d
Jobeth Jennifer Tampus <jobeth.tampus@nokia.com>
2016-03-30 11:13:21 +0800

    added socket config

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server, {
+    path: '/socket.io'
+});
+var socketServer = require('./backend/socketServer').connect(io);
 var port = process.env.PORT || 8080;
 
 app.use(express.static('app'));
 app.use(express.static('.'));
 
-app.listen(port);
-
-console.log("Server started with port", port);
+server.listen(port, function(){
+    console.log("Server started with port", port);
+});
